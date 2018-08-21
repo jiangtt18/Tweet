6fb7dff40f61c76c767d1c4807fabc7bda3a5306
jiangtt18 <tjiang90111@gmail.com>
2018-08-21 13:58:44 -0700

    set up controllers

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,17 @@
+class TweetsController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @tweets = Tweet.all.order(id: :desc).limit(25)
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/create'
+
+  get 'users/create'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
