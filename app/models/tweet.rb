07f48d40a7e98acb6285d182b20bb9cfcd7ec40e
jiangtt18 <tjiang90111@gmail.com>
2018-08-21 13:42:14 -0700

    setup models

---
+++ app/models/tweet.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :bigint(8)        not null, primary key
+#  body       :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Tweet < ApplicationRecord
+  validate :user_id, :body, presence: true
+
+  belongs_to :user
+end
