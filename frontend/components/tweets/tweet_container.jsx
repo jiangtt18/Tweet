f30ffb18ca2381ef43303d809c2f984c2ff843e1
jiangtt18 <tjiang90111@gmail.com>
2018-09-06 18:37:19 -0700

    long

--- frontend/components/tweets/tweet_container.jsx
+++ frontend/components/tweets/tweet_container.jsx
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-import {fetchAllTweets, deleteMyTweet, createMyTweet} from '../../actions/tweet_action.js';
+import {
+  fetchAllTweets,
+  deleteMyTweet,
+  createMyTweet} from '../../actions/tweet_action.js';
 import TweetIndex from './tweet_index.jsx';
 import {openModal} from '../../actions/modal_actions';
 
